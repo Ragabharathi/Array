2a78be977358db4bccc7095a547dbddf62bc3693
Ragabharathi <84565134+Ragabharathi@users.noreply.github.com>
2021-12-08 19:22:17 +0530

    Create Counting inversions.c

---
+++ Counting inversions.c
@@ -0,0 +1,42 @@
+Input: arr[] = {8, 4, 2, 1}
+Output: 6
+
+Explanation: Given array has six inversions:
+(8, 4), (4, 2), (8, 2), (8, 1), (4, 1), (2, 1).
+
+
+Input: arr[] = {3, 1, 2}
+Output: 2
+
+Explanation: Given array has two inversions:
+(3, 1), (3, 2) 
+  
+  Algorithm: 
+Traverse through the array from start to end
+For every element, find the count of elements smaller than the current number up to that index using another loop.
+Sum up the count of inversion for every index.
+Print the count of inversions.
+  
+  
+#include <stdio.h>
+#include <stdlib.h>
+int getInvCount(int arr[], int n)
+{
+    int inv_count = 0;
+    for (int i = 0; i < n - 1; i++)
+        for (int j = i + 1; j < n; j++)
+            if (arr[i] > arr[j])
+                inv_count++;
+ 
+    return inv_count;
+}
+ 
+
+int main()
+{
+    int arr[] = { 1, 20, 6, 4, 5 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf(" Number of inversions are %d \n",
+           getInvCount(arr, n));
+    return 0;
+}
